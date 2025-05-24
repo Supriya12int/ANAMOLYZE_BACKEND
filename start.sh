324da29d20fbee76cb787a4f1509c8d01a5358ae
Supriya12int <supriyajanji2005@gmail.com>
2025-05-24 11:05:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 # Install model using gdown
 echo "Downloading model..."
-gdown "$MODEL_URL" -O swin_transformer_trained.pth
+gdown --fuzzy "$MODEL_URL" -O swin_transformer_trained.pth
+ls -lh swin_transformer_trained.pth
+
 
 # Start Flask app
 echo "Starting Flask server..."
