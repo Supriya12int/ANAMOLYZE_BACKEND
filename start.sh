d8c19d2cc4ab83f930402d203e5123e7cbeab966
Supriya12int <supriyajanji2005@gmail.com>
2025-05-24 11:40:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Install model using gdown
 echo "Downloading model..."
-gdown --fuzzy "$MODEL_URL" -O swin_transformer_trained.pth
+gdown --fuzzy --id 1mpmYQdydILZLM82MRHB5WCa3oc_E7mZK -O swin_transformer_trained.pth
 ls -lh swin_transformer_trained.pth
 
 
